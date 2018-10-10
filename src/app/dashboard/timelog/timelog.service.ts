30e196f46a52b3266ffde7ecf195a20ea3a6e63c
Jeremy Sandau <jeremysandau@gmail.com>
2018-10-10 09:56:08 -0700

    Updating UI for timeMarks and set up initial fetching of timeMarks

--- src/app/dashboard/timelog/timelog.service.ts
+++ src/app/dashboard/timelog/timelog.service.ts
@@ -5,32 +5,45 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { TimeMark } from './time-mark.model';
 import { CategorizedActivity } from './categorized-activity.model';
 
+import * as moment from 'moment';
+
 import { serverUrl } from '../../serverurl';
 import { map } from 'rxjs/operators';
+import { AuthStatus } from '../../authentication/auth-status.model';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimelogService {
 
-  constructor(private httpClient: HttpClient, private authService: AuthenticationService) { }
+  constructor(private httpClient: HttpClient, private authService: AuthenticationService) {
+    authService.authStatus.subscribe((authStatus: AuthStatus) => {
+      if (authStatus.isAuthenticated) {
+        this.fetchTimeMarks(authStatus.user.id, moment().startOf('day').toISOString(), moment().endOf('day').toISOString());
+      } else {
+        this.logout();
+      }
+    })
+  }
 
   private serverUrl: string = serverUrl;
 
   private _timeMarksSubject: BehaviorSubject<TimeMark[]> = new BehaviorSubject<TimeMark[]>([]);
   // private _activitiesSubject: BehaviorSubject<CategorizedActivity> = new BehaviorSubject<CategorizedActivity>(null);
 
-  get timeMarks(): Observable<TimeMark[]>{
+  get timeMarks(): Observable<TimeMark[]> {
     return this._timeMarksSubject.asObservable();
   }
   // get activities(): Observable<CategorizedActivity>{
   //   return this._activitiesSubject.asObservable();
   // }
 
-  saveTimeMark(timeMark: TimeMark){
+  saveTimeMark(timeMark: TimeMark) {
     let newTimeMark: TimeMark = timeMark;
     newTimeMark.userId = this.authService.authenticatedUser.id;
-    
+
     const postUrl = this.serverUrl + "/api/timeMark/create";
     const httpOptions = {
       headers: new HttpHeaders({
@@ -46,14 +59,41 @@ export class TimelogService {
         return timeMark;
       }))
       .subscribe((timeMark: TimeMark) => {
-        console.log(timeMark);
+        console.log("http request:", timeMark);
         let timeMarks: TimeMark[] = this._timeMarksSubject.getValue();
         timeMarks.push(timeMark);
         this._timeMarksSubject.next(timeMarks);
       })
+  }
 
-  } 
+  private fetchTimeMarks(authenticatedUserId: string, startTime: string, endTime: string) {
+    const getUrl = this.serverUrl + "/api/timeMark/" + authenticatedUserId;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+        // 'Authorization': 'my-auth-token'  
+      })
+    };
+    this.httpClient.get<{ message: string, data: any }>(getUrl, httpOptions)
+      .pipe(map((response) => {
+        return response.data.map((dataObject) => {
+          let timeMark = new TimeMark(dataObject._id, dataObject.userId, dataObject.timeISO);
+          timeMark.description = dataObject.description;
+          timeMark.activities = dataObject.activities as CategorizedActivity[];
+          return timeMark;
+        })
+      }))
+      .subscribe((timeMarks: TimeMark[]) => {
+        console.log("get request:", timeMarks);
+        this._timeMarksSubject.next(timeMarks);
+      });
 
+  }
+
+
+  private logout() {
+    this._timeMarksSubject.next([]);
+  }
 
 
 }
