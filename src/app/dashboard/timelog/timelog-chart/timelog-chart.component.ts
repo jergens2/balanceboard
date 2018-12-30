8299e5e57056dcabcb6844a592b342ee721a1369
Jeremy Sandau <jeremysandau@gmail.com>
2018-12-29 23:30:02 -0800

    I don't know how to use git properly so I just manually copy and pasted the previous versions of timelog chart files. By doing so I am able to revert to the state that I want it in.  I probably should have made a separate branch before committing...

--- src/app/dashboard/timelog/timelog-chart/timelog-chart.component.ts
+++ src/app/dashboard/timelog/timelog-chart/timelog-chart.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { TimelogService } from '../timelog.service';
 import { TimeMark } from '../time-mark.model';
-import { ITimeMarkChartTile } from './timemark-chart-tile.interface';
 
 import * as moment from 'moment';
 import { ActivitiesService } from '../activities/activities.service';
+
+export interface ITimeMarkChartTile {
+  timeMark: TimeMark,
+  style: Object,
+  styleHeight: string,
+  styleMarginTop: string,
+  styleBackgroundColor: string,
+}
 
 
 
@@ -17,167 +24,119 @@ export class TimelogChartComponent implements OnInit {
 
   constructor(private timeLogService: TimelogService, private activitiesService: ActivitiesService) { }
 
-  chartHourTiles: ITimeMarkChartTile[] = [];
-  hours: string[] = [];
+  timeMarkChartTiles: ITimeMarkChartTile[];
+  hours: string[];
   hourHeight: number;
-  hourHeightPx: string;
-
-  frameStartHour: number;
-  frameEndHour: number;
 
   ngOnInit() {
 
-    this.hourHeight = 35;
-    this.hourHeightPx = this.hourHeight.toFixed(0) + "px";
-
-    this.frameStartHour = this.timeLogService.userDefinedStartTimeOfDay.hour();
-    if( this.timeLogService.userDefinedStartTimeOfDay.minute() >= 30){
-      this.frameStartHour += 1;
-    }
-
-    this.frameEndHour = this.timeLogService.userDefinedEndTimeOfDay.hour();
-    if (this.timeLogService.userDefinedStartTimeOfDay.minute() >= 30) {
-      this.frameEndHour += 1;
-    }
-
-    let hour: number = this.frameStartHour;
-    while (hour <= this.frameEndHour) {
-      this.hours.push(moment(this.timeLogService.userDefinedStartTimeOfDay).hour(hour).format('h a'));
+    /*
+      Much of the chart display appearance is statically defined in the CSS.  
+      Meaning: changing the start hour / end hour reference here would require specific changes to the pre-defined CSS grid.
+      at some point it may be wise to set the css programmatically
+      2018-12-16:  start and end time now exist as variables in the service, but this chart component still uses the statically defined CSS, 
+      and does not refer to the variables in the service to determine the css, but it should.
+    */
+    this.hourHeight = 28;
+    this.hours = [];
+    let hour = 8; //chart reference start time: 8:00am;
+    let endHour = 22;
+    while (hour <= endHour) {
+      this.hours.push(moment().hour(hour).format("h a"));
       hour++;
     }
 
-
-
-
+    this.timeMarkChartTiles = [];
     this.timeLogService.thisDaysTimeMarks.subscribe((timeMarks: TimeMark[]) => {
       let chartTimeMarks = Object.assign([], timeMarks);
-      this.chartHourTiles = this.buildHourTiles(chartTimeMarks, this.frameStartHour, this.frameEndHour);
-
-      // this.timeMarkChartTiles = this.buildTimeMarkChartTiles(chartTimeMarks, this.hourHeight);
+      this.timeMarkChartTiles = this.buildTimeMarkChartTiles(chartTimeMarks, this.hourHeight);
     })
   }
 
-  buildHourTiles(chartTimeMarks: TimeMark[], startHour: number, endHour: number): ITimeMarkChartTile[] {
-    /*
-      In the template, the hour labels are centered vertically.
-      This means that visually, the horizontal line is actually offset by half of an hour.
-      e.g. the horizontal line at the text 9am is not actually at 9am but at 9:30am.
-
-      Therefore, for each of these tiles, they actually need to run from -30 minutes to +30 minutes
-    */
-    console.log("building hour tiles");
-
+  buildTimeMarkChartTiles(timeMarks: TimeMark[], hourHeightPx: number): ITimeMarkChartTile[] {
     let tiles: ITimeMarkChartTile[] = [];
-    let hour: number = startHour;
-    while (hour <= endHour) {
-      
-      let startTime = moment().hour(hour).minute(-30).second(0).millisecond(0);
-      let endTime = moment().hour(hour).minute(30).second(0).millisecond(0);
 
-      let timeMarks: TimeMark[] = [];
+    if (timeMarks != null) {
+      timeMarks.sort((a, b) => {
+        if (moment(a.startTime).isBefore(moment(b.startTime))) {
+          return 1
+        } else {
+          return 0;
+        }
+      })
 
-      for(let timeMark of chartTimeMarks){
-        if(moment(timeMark.startTime).isAfter(startTime) || moment(timeMark.endTime).isBefore(endTime)){
-          timeMarks.push(Object.assign({},timeMark));
+      for (let timeMark of timeMarks) {
+
+        let templateStartTime = this.timeLogService.userDefinedStartTimeOfDay;
+        let templateEndTime = this.timeLogService.userDefinedEndTimeOfDay;
+
+        if (!moment(timeMark.endTime).isBefore(templateStartTime) && !moment(timeMark.startTime).isAfter(templateEndTime)) {
+
+          let marginTop = "1px";
+          let timeMarkHeight: string = "0px";
+          timeMarkHeight = (((timeMark.duration / 60) * hourHeightPx) - 1)  + "px";
+
+
+          if (moment(timeMark.startTime).isBefore(templateStartTime)) {
+            let hoursCut = moment(templateStartTime).diff((moment(timeMark.startTime)), 'hours');
+            let pxCut = hoursCut * hourHeightPx;
+
+            timeMarkHeight = (((timeMark.duration / 60) * hourHeightPx) - pxCut - 1) + "px";
+          }
+
+          if (moment(timeMark.endTime).isAfter(templateEndTime)) {
+            let endTime = moment(timeMark.endTime);
+
+            let hoursCut = moment(timeMark.endTime).diff((moment(templateEndTime)), 'hours');
+            let pxCut = hoursCut * hourHeightPx;
+            timeMarkHeight = (((timeMark.duration / 60) * hourHeightPx) - pxCut - 1) + "px";
+            // console.log("timemark height", timeMarkHeight);
+          }
+
+
+          let styleColor = "white";
+          if (timeMark.activities.length == 0) {
+            styleColor = "white";
+          } else if (timeMark.activities.length == 1) {
+
+
+            styleColor = timeMark.activities[0].activity.color
+          } else if (timeMark.activities.length > 1) {
+            // TODO need to calculate which activity represents the largest portion of the timeMark and return that activity's color
+            styleColor = timeMark.activities[0].activity.color
+          }
+          let tile: ITimeMarkChartTile = { timeMark: timeMark, style: {}, styleHeight: timeMarkHeight, styleBackgroundColor: styleColor, styleMarginTop: marginTop };
+          // console.log("pushing tile", tile);
+          tiles.push(tile);
         }
       }
-
-      let tile: ITimeMarkChartTile = { timeMarks: timeMarks, startTime: startTime, endTime: endTime};
-      tiles.push(tile);
-
-      hour++;
     }
-
-
     return tiles;
   }
 
-  // buildTimeMarkChartTiles(timeMarks: TimeMark[], hourHeightPx: number): ITimeMarkChartTile[] {
-  //   let tiles: ITimeMarkChartTile[] = [];
 
-  //   if (timeMarks != null) {
-  //     timeMarks.sort((a, b) => {
-  //       if (moment(a.startTime).isBefore(moment(b.startTime))) {
-  //         return 1
-  //       } else {
-  //         return 0;
-  //       }
-  //     })
+  calculateHeight(timeMark: TimeMark): {} {
+    let height = 0;
+    height = timeMark.duration;
+    let str = height.toString() + "px"
+    return { "height": str };
+  }
 
-  //     for (let timeMark of timeMarks) {
+  getTileHeight(timeMark: TimeMark): string {
+    let startTime = moment().hour(7).minute(30).second(0).millisecond(0);
+    let endTime = moment().hour(22).minute(30).second(0).millisecond(0);
+    let dayFrameDuration = moment.duration(moment(startTime).diff(moment(endTime))).asMinutes();
 
-  //       let templateStartTime = this.timeLogService.userDefinedStartTimeOfDay;
-  //       let templateEndTime = this.timeLogService.userDefinedEndTimeOfDay;
+    dayFrameDuration = Math.abs(dayFrameDuration);
 
-  //       if (!moment(timeMark.endTime).isBefore(templateStartTime) && !moment(timeMark.startTime).isAfter(templateEndTime)) {
+    let frameHeight = 500; //500px
 
-  //         let marginTop = "1px";
-  //         let timeMarkHeight: string = "0px";
-  //         timeMarkHeight = (((timeMark.duration / 60) * hourHeightPx) - 1)  + "px";
+    let timeMarkHeight = timeMark.duration / (dayFrameDuration / frameHeight);
 
-
-  //         if (moment(timeMark.startTime).isBefore(templateStartTime)) {
-  //           let hoursCut = moment(templateStartTime).diff((moment(timeMark.startTime)), 'hours');
-  //           let pxCut = hoursCut * hourHeightPx;
-
-  //           timeMarkHeight = (((timeMark.duration / 60) * hourHeightPx) - pxCut - 1) + "px";
-  //         }
-
-  //         if (moment(timeMark.endTime).isAfter(templateEndTime)) {
-  //           let endTime = moment(timeMark.endTime);
-
-  //           let hoursCut = moment(timeMark.endTime).diff((moment(templateEndTime)), 'hours');
-  //           let pxCut = hoursCut * hourHeightPx;
-  //           timeMarkHeight = (((timeMark.duration / 60) * hourHeightPx) - pxCut - 1) + "px";
-  //           // console.log("timemark height", timeMarkHeight);
-  //         }
-
-
-  //         let styleColor = "white";
-  //         if (timeMark.activities.length == 0) {
-  //           styleColor = "white";
-  //         } else if (timeMark.activities.length == 1) {
-
-
-  //           styleColor = timeMark.activities[0].activity.color
-  //         } else if (timeMark.activities.length > 1) {
-  //           // TODO need to calculate which activity represents the largest portion of the timeMark and return that activity's color
-  //           styleColor = timeMark.activities[0].activity.color
-  //         }
-  //         let tile: ITimeMarkChartTile = { timeMark: timeMark, style: {}, styleHeight: timeMarkHeight, styleBackgroundColor: styleColor, styleMarginTop: marginTop };
-  //         // console.log("pushing tile", tile);
-  //         tiles.push(tile);
-  //       }
-  //     }
-  //   }
-  //   return tiles;
-  // }
-
-
-  // calculateHeight(timeMark: TimeMark): {} {
-  //   let height = 0;
-  //   height = timeMark.duration;
-  //   let str = height.toString() + "px"
-  //   return { "height": str };
-  // }
-
-  // getTileHeight(timeMark: TimeMark): string {
-  //   let startTime = moment().hour(7).minute(30).second(0).millisecond(0);
-  //   let endTime = moment().hour(22).minute(30).second(0).millisecond(0);
-  //   let dayFrameDuration = moment.duration(moment(startTime).diff(moment(endTime))).asMinutes();
-
-  //   dayFrameDuration = Math.abs(dayFrameDuration);
-
-  //   let frameHeight = 500; //500px
-
-  //   let timeMarkHeight = timeMark.duration / (dayFrameDuration / frameHeight);
-
-  //   let heightStyle = timeMarkHeight.toFixed(0) + "px";
-  //   return heightStyle;
-  //   // return { 'height': heightStyle };
-  // }
-
-
+    let heightStyle = timeMarkHeight.toFixed(0) + "px";
+    return heightStyle;
+    // return { 'height': heightStyle };
+  }
 
 
   /*
