727c0b459f3cbc3f4b3ebfbd6432a3914e4b79bf
Jeremy Sandau <jeremysandau@gmail.com>
2019-01-05 23:20:13 -0800

    daybook in good state.  now time is showing in the margin

--- src/app/dashboard/daybook/daybook.component.ts
+++ src/app/dashboard/daybook/daybook.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import * as moment from 'moment';
 import { TimelogService } from '../timelog/timelog.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-daybook',
@@ -19,13 +20,18 @@ export class DaybookComponent implements OnInit {
   daybookBodyStyle: any = { "background-color": "red", "border": "10px solid green" };
   hourLabels: any[] = [];
   bookLines: any[] = [];
+  nowLineContainerStyle: any = {};
+  nowTime: moment.Moment = moment();
+  nowTimeContainerStyle: any = {};
+
+  nowSubscription: Subscription = new Subscription();
 
   ngOnInit() {
 
 
     this.timeLogService.currentDate$.subscribe((changedDate: moment.Moment) => {
       this._currentDate = moment(changedDate);
-      this.dayStartTime = moment(this._currentDate).hour(6).minute(30).second(0).millisecond(0);
+      this.dayStartTime = moment(this._currentDate).hour(7).minute(30).second(0).millisecond(0);
       this.dayEndTime = moment(this._currentDate).hour(22).minute(30).second(0).millisecond(0);
 
       this.buildDisplay();
@@ -40,9 +46,9 @@ export class DaybookComponent implements OnInit {
     hour = this.dayStartTime.hour();
 
     let currentTime = moment(this.dayStartTime).subtract(30, 'minutes');
-    if (currentTime.minute() < 15) {
+    if (currentTime.minute() <= 15) {
       currentTime.minute(0);
-    } else if (currentTime.minute() >= 15 && currentTime.minute() < 45) {
+    } else if (currentTime.minute() > 15 && currentTime.minute() <= 45) {
       currentTime.minute(30);
     } else {
       currentTime.minute(60);
@@ -54,7 +60,33 @@ export class DaybookComponent implements OnInit {
     let gridLines: any[] = [];
 
     let gridIndex: number = 1;
+
+    let now = moment();
+    let nowLineStyle: any = {};
+    let nowTimeContainerStyle: any = {};
+
     while (currentTime.isSameOrBefore(endTime)) {
+
+      let segmentEnd = moment(currentTime).add(30,'minutes');
+      if(moment(now).isAfter(currentTime) && moment(now).isBefore(segmentEnd)){
+        
+        //need to update the now with an observable to move every so many minutes
+        let nowLinePercentage: number = 1;
+        let lineSeconds = moment(now).diff(moment(currentTime), 'seconds');
+        
+
+        nowLinePercentage = ((lineSeconds / (30 * 60)) * 100);
+        let lineGridTemplateRows = "" + nowLinePercentage.toFixed(0) + "% " +  (100-nowLinePercentage).toFixed(0) + "%";
+        nowLineStyle = { "grid-row":"" + gridIndex + " / span 1", "grid-template-rows": lineGridTemplateRows };
+
+        let nowTimePercentage: number = 1;
+        let nowTimeSeconds = moment(now).diff(moment(currentTime).subtract(30,'minutes'), 'seconds');
+
+        nowTimePercentage = ((nowTimeSeconds / (30 * 60 * 3)) * 100);
+        let nowTimeGridTemplateRows = "" + nowTimePercentage.toFixed(0) + "%" + (100-nowTimePercentage).toFixed(0) + "%";
+
+        nowTimeContainerStyle = { "grid-row":"" + (gridIndex-1) + " / span 3", "grid-column":"1 / span 1", "grid-template-rows": nowTimeGridTemplateRows };
+      }
 
       let gridLine = {
         "line": gridIndex,
@@ -66,7 +98,7 @@ export class DaybookComponent implements OnInit {
 
         let hourLabel = {
           "time": currentTime.format("h:mm a"),
-          "style": { "grid-column": "1 / span 2", "grid-row": "" + gridIndex + " / span 2" }
+          "style": { "grid-column": "1 / span 1", "grid-row": "" + gridIndex + " / span 2" }
         };
         if(gridIndex == 1 ){
           hourLabel = {
@@ -74,16 +106,21 @@ export class DaybookComponent implements OnInit {
             "style": { "grid-column": "1 / span 2", "grid-row": "" + gridIndex + " / span 2" }
           };
         }
-
         hourLabels.push(hourLabel);
+        
       }
       currentTime.add(30, 'minutes');
       gridIndex += 1;
     }
 
-    this.daybookBodyStyle = { "display":"grid", "grid-template-rows": "repeat(" + gridIndex.toFixed(0) + ", 1fr)", "grid-template-columns": "5em 6px auto" }
+    
+    this.daybookBodyStyle = { "display":"grid", "grid-template-rows": "repeat(" + gridIndex.toFixed(0) + ", 1fr)" }
     this.hourLabels = hourLabels;
     this.bookLines = gridLines;
+
+    this.nowLineContainerStyle = nowLineStyle;
+    this.nowTimeContainerStyle = nowTimeContainerStyle;
+    this.nowTime = now;
   }
 
 
