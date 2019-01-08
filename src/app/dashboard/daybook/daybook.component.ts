4ac0876c87bc68dc967c4b3ac838a85ba44dd856
Jeremy Sandau <jeremysandau@gmail.com>
2019-01-07 23:14:55 -0800

    bedtime marker working with good css and programmed styling

--- src/app/dashboard/daybook/daybook.component.ts
+++ src/app/dashboard/daybook/daybook.component.ts
@@ -33,6 +33,37 @@ export class DaybookComponent implements OnInit {
   nowTimeContainerStyle: any = {};
 
   bedTimeStyle: any = {};
+  bedTimeString: string = "";
+
+  calculateBedTimeString(now: moment.Moment): string{
+    let bedTimeString = "";
+    let minutes = Math.floor(moment(now).diff(moment(this.bedTime),'minutes'));
+    if(minutes < -1){
+      if(minutes > -180){
+        let hours = Math.floor(minutes / 60) 
+        let remainingMinutes = minutes - (hours * 60);
+        bedTimeString = "" + hours + " hours and " + remainingMinutes + " minutes until bedtime";
+      }else{
+        bedTimeString = "Bedtime: " + this.bedTime.format('hh:mm a');
+      }
+    }else if(minutes < 1){
+      bedTimeString = "It's bedtime.";
+    }else{
+      if(minutes < 60){
+        bedTimeString = "It's " + minutes + " minutes past bedtime";
+      }else{
+        let hours = Math.floor(minutes / 60);
+        let remainingMinutes = minutes - (hours * 60);
+        if(hours == 1){
+          bedTimeString = "It's 1 hour and " + remainingMinutes + " minutes past bedtime.";
+        }else{
+          bedTimeString = "It's " + hours + " hours and " + remainingMinutes + " minutes past bedtime.";
+        }
+      }
+      
+    }
+    return bedTimeString ;
+  }
 
   nowSubscription: Subscription = new Subscription();
 
@@ -42,18 +73,21 @@ export class DaybookComponent implements OnInit {
 
 
     this.timeLogService.currentDate$.subscribe((changedDate: moment.Moment) => {
-      this._currentDate = moment(changedDate);
+      if(changedDate != null){
+        this._currentDate = moment(changedDate);
 
-      // this.fallAsleepTime = moment(this._currentDate).hour(23).minute(30).second(0).millisecond(0);
-      this.bedTime = moment(this._currentDate).hour(22).minute(30).second(0).millisecond(0);
-
-      this.dayStartTime = moment(this._currentDate).hour(7).minute(30).second(0).millisecond(0);
-      this.dayEndTime = moment(this._currentDate).hour(22).minute(30).second(0).millisecond(0);
-
-      this.nowSubscription.unsubscribe();
-      this.nowSubscription = timer(0,60000).subscribe(()=>{
-        this.buildDisplay();
-      })
+        // this.fallAsleepTime = moment(this._currentDate).hour(23).minute(30).second(0).millisecond(0);
+        this.bedTime = moment(this._currentDate).hour(22).minute(30).second(0).millisecond(0);
+  
+        this.dayStartTime = moment(this._currentDate).hour(7).minute(30).second(0).millisecond(0);
+        this.dayEndTime = moment(this._currentDate).hour(22).minute(30).second(0).millisecond(0);
+  
+        this.nowSubscription.unsubscribe();
+        this.nowSubscription = timer(0,60000).subscribe(()=>{
+          this.buildDisplay();
+        })
+      }
+      
       
     })
 
@@ -83,33 +117,31 @@ export class DaybookComponent implements OnInit {
     let gridIndex: number = 1;
 
     let now = moment();
-    let nowLineStyle: any = {};
+    let nowLineContainerStyle: any = {};
     let nowTimeContainerStyle: any = {};
+    let bedTimeStyle: any = {};
+
+    function getGridTemplateRowsStyle(referenceTime: moment.Moment, currentSegmentTime: moment.Moment, rows: number): string {
+      let percentage: number = 1;
+      let seconds = moment(referenceTime).diff(moment(currentSegmentTime), 'seconds');
+      
+
+      percentage = ((seconds / (30 * 60 * rows)) * 100);
+      let gridTemplateRows = "" + percentage.toFixed(0) + "% " +  (100-percentage).toFixed(0) + "%";
+      return gridTemplateRows;
+    }
 
     while (currentTime.isSameOrBefore(endTime)) {
-
       /*
         There is a bug where if now is greater than dayEndTime then this following if block does not fire and the now line does not display properly. 
       */
       let segmentEnd = moment(currentTime).add(30,'minutes');
-      if(moment(now).isAfter(currentTime) && moment(now).isBefore(segmentEnd)){
-        
-        //need to update the now with an observable to move every so many minutes
-        let nowLinePercentage: number = 1;
-        let lineSeconds = moment(now).diff(moment(currentTime), 'seconds');
-        
-
-        nowLinePercentage = ((lineSeconds / (30 * 60)) * 100);
-        let lineGridTemplateRows = "" + nowLinePercentage.toFixed(0) + "% " +  (100-nowLinePercentage).toFixed(0) + "%";
-        nowLineStyle = { "grid-row":"" + gridIndex + " / span 1", "grid-template-rows": lineGridTemplateRows };
-
-        let nowTimePercentage: number = 1;
-        let nowTimeSeconds = moment(now).diff(moment(currentTime).subtract(30,'minutes'), 'seconds');
-
-        nowTimePercentage = ((nowTimeSeconds / (30 * 60 * 3)) * 100);
-        let nowTimeGridTemplateRows = "" + nowTimePercentage.toFixed(0) + "%" + (100-nowTimePercentage).toFixed(0) + "%";
-
-        nowTimeContainerStyle = { "grid-row":"" + (gridIndex-1) + " / span 3", "grid-column":"1 / span 1", "grid-template-rows": nowTimeGridTemplateRows };
+      if(moment(now).isAfter(currentTime) && moment(now).isSameOrBefore(segmentEnd)){
+        nowLineContainerStyle = { "grid-row":"" + gridIndex + " / span 1", "grid-template-rows": getGridTemplateRowsStyle(now, currentTime, 1) };
+        nowTimeContainerStyle = { "grid-row":"" + (gridIndex-1) + " / span 3", "grid-column":"1 / span 1", "grid-template-rows": getGridTemplateRowsStyle(now, moment(currentTime).subtract(30,'minutes'), 3) };
+      }
+      if(moment(this.bedTime).isAfter(currentTime) && moment(this.bedTime).isSameOrBefore(segmentEnd)){
+        bedTimeStyle = { "grid-row":"" + (gridIndex+1) + " / -1 ", "grid-template-rows": getGridTemplateRowsStyle(this.bedTime, currentTime, 1)}
       }
 
       let gridLine = {
@@ -119,7 +151,6 @@ export class DaybookComponent implements OnInit {
       gridLines.push(gridLine);
 
       if (currentTime.minute() != 30) {
-
         let hourLabel = {
           "time": currentTime.format("h a"),
           "style": { "grid-column": "1 / span 1", "grid-row": "" + gridIndex + " / span 2" }
@@ -131,7 +162,6 @@ export class DaybookComponent implements OnInit {
           };
         }
         hourLabels.push(hourLabel);
-        
       }
       currentTime.add(30, 'minutes');
       gridIndex += 1;
@@ -142,10 +172,16 @@ export class DaybookComponent implements OnInit {
     this.hourLabels = hourLabels;
     this.bookLines = gridLines;
 
-    this.nowLineContainerStyle = nowLineStyle;
+    this.nowLineContainerStyle = nowLineContainerStyle;
     this.nowTimeContainerStyle = nowTimeContainerStyle;
     this.nowTime = now;
+
+
+    this.bedTimeString = this.calculateBedTimeString(now);
+
+    this.bedTimeStyle = bedTimeStyle;
   }
+
 
 
   ngOnDestroy(){
