cf9bc4d2de2603836a85106088f785cf6ffc571e
Jeremy Sandau <jeremysandau@gmail.com>
2019-07-22 15:54:11 -0700

    can delete activities

--- src/app/dashboard/activities/activity/activity.component.ts
+++ src/app/dashboard/activities/activity/activity.component.ts
@@ -5,6 +5,8 @@ import { faEdit } from '@fortawesome/free-regular-svg-icons';
 import { Modal } from '../../../modal/modal.class';
 import { ModalComponentType } from '../../../modal/modal-component-type.enum';
 import { ModalService } from '../../../modal/modal.service';
+import { IModalOption } from '../../../modal/modal-option.interface';
+import { ActivityCategoryDefinitionService } from '../../../shared/document-definitions/activity-category-definition/activity-category-definition.service';
 
 @Component({
   selector: 'app-activity',
@@ -16,7 +18,7 @@ export class ActivityComponent implements OnInit {
   faTimes = faTimes;
   faEdit = faEdit;
 
-  constructor(private modalService: ModalService) { }
+  constructor(private modalService: ModalService, private activitiesService: ActivityCategoryDefinitionService) { }
 
   @Input() activity: ActivityCategoryDefinition;
 
@@ -41,14 +43,27 @@ export class ActivityComponent implements OnInit {
   }
 
   onClickDeleteActivity(){
-    let modal: Modal = new Modal("Delect Activity: "+this.activity.name + "?", "", null, [], null, ModalComponentType.ActivityCategoryDefinition)
+    let modalOptions: IModalOption[] = [
+      {
+        value: "Delete",
+        dataObject: null,
+      },
+      {
+        value: "Cancel",
+        dataObject: null,
+      }
+
+    ]
+    let modal: Modal = new Modal("Delect Activity: "+this.activity.name + "?", "", null, modalOptions, null, ModalComponentType.Confirm);
     this.modalService.activeModal = modal;
     this.modalService.modalResponse$.subscribe((response) => {
-      console.log("modal response:", response);
+      if(response.value == "Delete"){
+        this.activitiesService.deleteActivity(this.activity);
+      }
     });
   }
   onClickEditActivity(){
-    let modal: Modal = new Modal("Edit Activity "+this.activity.name, "", this.activity, [], null, ModalComponentType.ActivityCategoryDefinition)
+    let modal: Modal = new Modal("Edit Activity "+this.activity.name, "", this.activity, [], null, ModalComponentType.ActivityCategoryDefinition);
     this.modalService.activeModal = modal;
     this.modalService.modalResponse$.subscribe((response) => {
       console.log("modal response:", response);
